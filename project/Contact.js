07d4031d36ae1c649eed5a05619cba6615d886d5
abdallahijjawe <149177409+abdallahijjawe@users.noreply.github.com>
2024-08-28 18:21:23 +0300

    Add files via upload

---
+++ project/Contact.js
@@ -0,0 +1,11 @@
+function DONE() {
+    a=confirm("Was that enough?")
+
+    if(a){
+        alert("Thank you for contacting us.")
+    }
+    else{
+        alert("We are sorry for that, you can visit us at our location or call 0779550604")
+    }
+}
+
